ac48c6fa7d59451f1e513472d85c4e8e55fe356d
Tsjippy <ewaldha@hotmail.com>
2024-10-11 07:01:24 +0100

    changed: enqueing of styles and scripts

--- php/enqueue.php
+++ php/enqueue.php
@@ -5,19 +5,19 @@ use SIM;
 add_action( 'wp_enqueue_scripts', function(){
     if(!is_user_logged_in()){
 	    //login form
-	    wp_register_style( 'sim_login_style', plugins_url('css/login.min.css', __DIR__), array(), MODULE_VERSION);
+	    wp_register_style( 'sim_login_style', SIM\pathToUrl(MODULE_PATH.'css/login.min.css'), array(), MODULE_VERSION);
         wp_enqueue_style( 'sim_login_style');
 
-        wp_enqueue_script('sim_login_script', plugins_url('js/login.min.js', __DIR__), array('sim_script', 'sim_purify'), MODULE_VERSION, true);
+        wp_enqueue_script('sim_login_script', SIM\pathToUrl(MODULE_PATH.'js/login.min.js'), array('sim_script', 'sim_purify'), MODULE_VERSION, true);
     }else{
-        wp_enqueue_script('sim_logout_script', plugins_url('js/logout.min.js', __DIR__), array(), MODULE_VERSION, true);
+        wp_enqueue_script('sim_logout_script', SIM\pathToUrl(MODULE_PATH.'js/logout.min.js'), array(), MODULE_VERSION, true);
     }
 
-    wp_register_style( 'sim_pw_reset_style', plugins_url('css/pw_reset.min.css', __DIR__), array(), MODULE_VERSION);
+    wp_register_style( 'sim_pw_reset_style', SIM\pathToUrl(MODULE_PATH.'css/pw_reset.min.css'), array(), MODULE_VERSION);
 
-    wp_register_script('sim_password_strength_script', plugins_url('js/password_strength.min.js', __DIR__), array('password-strength-meter'), MODULE_VERSION,true);
+    wp_register_script('sim_password_strength_script', SIM\pathToUrl(MODULE_PATH.'js/password_strength.min.js'), array('password-strength-meter'), MODULE_VERSION,true);
 
-	wp_register_script('sim_2fa_script', plugins_url('js/2fa.min.js', __DIR__), array('sim_table_script'), MODULE_VERSION, true);
+	wp_register_script('sim_2fa_script', SIM\pathToUrl(MODULE_PATH.'js/2fa.min.js'), array('sim_table_script'), MODULE_VERSION, true);
 
     if(is_numeric(get_the_ID())){
         $passwordResetPage  = SIM\getModuleOption(MODULE_SLUG, 'password_reset_page');
