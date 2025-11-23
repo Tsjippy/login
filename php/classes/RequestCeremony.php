c46603d34289a011b816f835b4f0769db5dcd6b2
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2025-11-23 18:31:32 +0100

    Create RequestCeremony.php

---
+++ php/classes/RequestCeremony.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace LOGIN;
+
+/**
+* Register a webauthn method
+*/
+class RequestCeremony{
+    public function __construct(){
+        
+    }
+    
+    public function createOptions(){
+    }
+    
+    public function verifyResponse(){
+        
+    }
+}
