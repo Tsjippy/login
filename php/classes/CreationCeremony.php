405a79c60b112798ee0c9e5da443e9dd42979866
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2025-11-23 18:30:52 +0100

    Create CreationCeremony.php

---
+++ php/classes/CreationCeremony.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace LOGIN;
+
+/**
+* Register a webauthn method
+*/
+class CreationCeremony{
+    public function __construct(){
+        
+    }
+    
+    public function createOptions(){
+    }
+    
+    public function verifyResponse(){
+        
+    }
+}
