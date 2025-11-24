fc91a262847a63a4a42cb0e8585913652b05f3d9
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2025-11-24 01:56:01 +0100

    Update WebAuthCeremony.php

--- php/classes/WebAuthCeremony.php
+++ php/classes/WebAuthCeremony.php
@@ -9,6 +9,7 @@ use Webauthn\AttestationStatement\NoneAttestationStatementSupport;
 use Webauthn\Denormalizer\WebauthnSerializerFactory;
 use Webauthn\PublicKeyCredential;
 use Webauthn\CeremonyStep\CeremonyStepManagerFactory;
+use DeviceDetector\Parser\OperatingSystem as OS_info;
 
 /**
 * Register a webauthn method
@@ -22,6 +23,7 @@ class WebAuthCeremony{
     public $factory;
     public $user;
     public $credentials;
+    public $domain;
     
     public function __construct(){
         $this->user = wp_get_current_user();
@@ -35,8 +37,9 @@ class WebAuthCeremony{
         $factory = new WebauthnSerializerFactory($attestationStatementSupportManager);
         $this->serializer = $factory->create();
         
-        
         $this->factory = new CeremonyStepManagerFactory();
+        
+        $this->domain = $_SERVER['SERVER_NAME'];
     }
     
     /**
@@ -57,7 +60,7 @@ class WebAuthCeremony{
     
         return $this->rpEntity = new PublicKeyCredentialRpEntity(
             get_bloginfo('name').' Webauthn Server', // The application name
-            $_SERVER['SERVER_NAME'],       // The application ID = the domain
+            $this->domain,       // The application ID = the domain
             //$logo
             //picture from example on https://webauthn-doc.spomky-labs.com/prerequisites/the-relying-party , does not work
             'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAMAAAC6V+0/AAAAwFBMVEXm7NK41k3w8fDv7+q01Tyy0zqv0DeqyjOszDWnxjClxC6iwCu11z6y1DvA2WbY4rCAmSXO3JZDTxOiwC3q7tyryzTs7uSqyi6tzTCmxSukwi9aaxkWGga+3FLv8Ozh6MTT36MrMwywyVBziSC01TbT5ZW9z3Xi6Mq2y2Xu8Oioxy7f572qxzvI33Tb6KvR35ilwTmvykiwzzvV36/G2IPw8O++02+btyepyDKvzzifvSmw0TmtzTbw8PAAAADx8fEC59dUAAAA50lEQVQYV13RaXPCIBAG4FiVqlhyX5o23vfVqUq6mvD//1XZJY5T9xPzzLuwgKXKslQvZSG+6UXgCnFePtBE7e/ivXP/nRvUUl7UqNclvO3rpLqofPDAD8xiu2pOntjamqRy/RqZxs81oeVzwpCwfyA8A+8mLKFku9XfI0YnSKXnSYZ7ahSII+AwrqoMmEFKriAeVrqGM4O4Z+ADZIhjg3R6LtMpWuW0ERs5zunKVHdnnnMLNQqaUS0kyKkjE1aE98b8y9x9JYHH8aZXFMKO6JFMEvhucj3Wj0kY2D92HlHbE/9Vk77mD6srRZqmVEAZAAAAAElFTkSuQmCC'
